4582023899525bd1b2aa40411f95c02e0676b251
Paulomeister <71011789+grabakin@users.noreply.github.com>
2025-09-06 20:11:32 +0300

    реф:убрал текст, добавил хеадер

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+﻿import {useState} from "react";
+
+const Header = () => {
+    const [balance, setBalance] = useState<number>(100)
+
+    const updateBalance = (amount: number) => {
+        setBalance(prev => prev + amount);
+    };
+    return (
+        <header></header>
+    )
+
+}
+export default Header
